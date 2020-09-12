19e6b9ec5e6ce62945e48e420fb6e51ee24987f4
Michael Spremulli <michaels.wpi@gmail.com>
2020-09-11 22:28:07 -0400

    Created Quote class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Quote quote1 = new Quote("site1", (float)500.40);
+        quote1.printQuote();
     }
 }
--- src/com/company/Quote.java
+++ src/com/company/Quote.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Quote {
+    private String website;
+    private float price;
+
+    public Quote(String website, float price) {
+        this.website = website;
+        this.price = price;
+    }
+
+    private String getWebsite() {
+        return website;
+    }
+
+    private float getPrice() {
+        return price;
+    }
+
+    public void printQuote(){
+        System.out.println("Quote{site="+ getWebsite() + ", price:" + getPrice() + "}");
+    }
+
 }
